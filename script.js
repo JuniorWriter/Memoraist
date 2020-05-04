ca5c0153fd0239dff7b8bbd51ddbe8fd5694977e
JuniorWriter <62343874+JuniorWriter@users.noreply.github.com>
2020-05-04 16:20:45 -0500

    Primera versión de Memoraist (menú)

---
+++ script.js
@@ -0,0 +1,47 @@
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio('Resources/Audio/Rollinat5.mp3');
+        this.matchSound = new Audio('Resources/Audio/match.wav');
+        this.bgMusic.volume = 0.5;
+        this.bgMusic.loop = true;
+    }
+    startMusic() {
+        this.bgMusic.play();
+    }
+    stopMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+}
+
+class Memoraist {
+    constructor(_audition) {
+        this.audioController = new AudioController();
+    }
+
+    startGame() {
+        setTimeout(() => {
+            this.audioController.startMusic();
+        }, 500)
+    }
+}
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new Memoraist (100, cards);
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            game.startGame();
+        });
+    });
+
+}
